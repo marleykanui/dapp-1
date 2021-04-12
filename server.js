f4be5c42e137cfd703805f8410a685863643cef0
Kanui <marley.kanui@gmail.com>
2021-04-12 18:51:24 -0500

    dapp-1 - global commit 14 - linked with paypal manager

--- server.js
+++ server.js
@@ -28,7 +28,54 @@ app.post('/post_info', async (req, res) => {
     return res.send(return_info);
   }
   let result = await save_user_information({ amount: amount, email: email });
-  res.send(result);
+
+  var create_payment_json = {
+    intent: 'sale',
+    payer: {
+      payment_method: 'paypal',
+    },
+    redirect_urls: {
+      return_url: 'http://localhost:3000/success',
+      cancel_url: 'http://localhost:3000/cancel',
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: 'Lottery',
+              sku: 'Funding',
+              price: amount,
+              currency: 'USD',
+              quantity: 1,
+            },
+          ],
+        },
+        amount: {
+          currency: 'USD',
+          total: amount,
+        },
+        payee: {
+          email: 'sb-dzry65906043@business.example.com',
+        },
+        description: 'Lottery Purchase',
+      },
+    ],
+  };
+
+  paypal.payment.create(create_payment_json, (error, payment) => {
+    if (error) {
+      throw error;
+    } else {
+      console.log('Create Payment Response');
+      console.log(payment);
+      for (let i = 0; i < payment.links.length; i++) {
+        if (payment.links[i].rel === 'approval_url') {
+          return res.send(payment.links[i].href);
+        }
+      }
+    }
+  });
 });
 
 app.get('/get_total_amount', async (req, res) => {
