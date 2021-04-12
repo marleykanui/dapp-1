65577d584f9693625351a60e7a300a729bc01561
Kanui <marley.kanui@gmail.com>
2021-04-12 17:04:13 -0500

    dapp-1 - global commit 10 - Added input field to frontend

--- public/index.js
+++ public/index.js
@@ -1,11 +1,22 @@
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      total_amount: 1000,
+    };
   }
   render() {
     return (
       <div>
         <h1>Lottery Web 2.0</h1>
+        <div>
+          <p>Total Lottery Amount is {this.state.total_amount}</p>
+        </div>
+        <form action="">
+          <input type="text" placeholder="amount" />
+          <input type="text" placeholder="email" />
+          <button>Participate</button>
+        </form>
       </div>
     );
   }
